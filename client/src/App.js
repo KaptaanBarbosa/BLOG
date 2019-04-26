4aec4667dc83a4d11a7814b909ddd9feb6e25492
Siddharth Shrivastava <siddharth.shrivastava@ascenaretail.com>
2019-04-26 16:25:28 +0530

    adding client code structure

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      </header>
+      <h1>Site under Construction .....</h1>
+    </div>
+  );
+}
+
+export default App;
